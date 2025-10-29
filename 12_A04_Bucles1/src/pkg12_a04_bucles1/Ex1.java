0751ad7c53db8f074bf8f2b7dad44695b91152fd
jrubio55 <registres17@gmail.com>
2025-10-29 19:54:38 +0100

    numero % 3 == 0 ac1 1 i 2

--- 12_A04_Bucles1/src/pkg12_a04_bucles1/Ex1.java
+++ 12_A04_Bucles1/src/pkg12_a04_bucles1/Ex1.java
@@ -6,8 +6,40 @@ public class Ex1 {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        
-        
+        int numero = 0, suma = 1, contador = 1;
+        boolean numeroValid = false;
+
+        while (!numeroValid) {
+            System.out.println("Dis-me el número: ");
+
+            if (sc.hasNextInt()) {
+                numero = sc.nextInt();
+                if (numero > 0) {
+                    numeroValid = true;
+//                    // JA TINC UN VALOR ENTER POSITIU
+//                    while (contador < numero) {
+//                        //contador++;
+//                        //suma += contador; // suma = suma + contador;
+//                        suma += ++contador;
+//                    }
+//                    System.out.println(suma);
+                } else {
+                    System.out.println("Ha de ser un número enter i positiu!");
+                }
+            } else {
+                System.out.println("Ha de ser un número!");
+            }
+            sc.nextLine();
+        }
+
+        // JA TINC UN VALOR ENTER POSITIU
+        while (contador < numero) {
+            //contador++;
+            //suma += contador; // suma = suma + contador;
+            suma += ++contador;
+        }
+        System.out.println(suma);
+
     }
-    
+
 }

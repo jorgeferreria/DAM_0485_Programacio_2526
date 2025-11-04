3014046627cff11ae6e45b1a4b6fae0f0bab9647
jrubio55 <registres17@gmail.com>
2025-11-04 19:36:43 +0100

    12_A04B_Bucles1 Act1 v2

--- 12_A04B_Bucles1/src/pkg12_a04b_bucles1/Ex1.java
+++ 12_A04B_Bucles1/src/pkg12_a04b_bucles1/Ex1.java
@@ -6,16 +6,16 @@ public class Ex1 {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int numero = 0;
+        int totalCarbasses = 0;
         double pes = 0, pesTotal = 0;
         boolean numeroValid = false;
 
         do {
-            System.out.println("Dis-me el número: ");
+            System.out.println("Dis-me el total de carbasses trobades: ");
 
             if (sc.hasNextInt()) {
-                numero = sc.nextInt();
-                if (numero > 0) {
+                totalCarbasses = sc.nextInt();
+                if (totalCarbasses >= 0) {
                     numeroValid = true;
                 } else {
                     System.out.println("Ha de ser un número enter i positiu!");
@@ -23,32 +23,41 @@ public class Ex1 {
             } else {
                 System.out.println("Ha de ser un número!");
             }
+            // Buidar Buffer
             sc.nextLine();
         } while (!numeroValid);
-        // numero vàlid!!!
-        for (int i = 0; i < numero; i++) {
-            numeroValid = false;
-            do {
-                System.out.println("Dis-me el pes de la carbassa: ");
-
-                if (sc.hasNextDouble()) {
-                    pes = sc.nextDouble();
-                    if (pes > 0) {
-                        numeroValid = true;
-                        //suma += pes; // BÉ
-                    } else {
-                        System.out.println("Ha de ser un número enter i positiu!");
-                    }
-                } else {
-                    System.out.println("Ha de ser un número!");
-                }
-                sc.nextLine();
-            } while (!numeroValid);
-            pesTotal += pes;
-        }
         
-        System.out.println("El pes total de les carbasse és: " + pesTotal);
-        System.out.printf("La mitaja de pes és: %.1f\n", pesTotal / numero);
+        // totalCarbasses vàlid!!!
+        if (totalCarbasses > 0) {
+
+            for (int i = 0; i < totalCarbasses; i++) {
+                numeroValid = false;
+                
+                do {
+                    System.out.println("Dis-me el pes de la carbassa "+ (i+1) +": " );
+
+                    if (sc.hasNextDouble()) {
+                        pes = sc.nextDouble();
+                        if (pes > 0) {
+                            numeroValid = true;
+                            //suma += pes; // BÉ
+                        } else {
+                            System.out.println("Ha de ser un número enter i positiu!");
+                        }
+                    } else {
+                        System.out.println("Ha de ser un número!");
+                    }
+                    sc.nextLine();
+                } while (!numeroValid);
+                
+                pesTotal += pes;
+            }
+
+            System.out.println("El pes total de les carbasse és: " + pesTotal);
+            System.out.printf("La mitaja de pes és: %.1f\n", pesTotal / totalCarbasses);
+        } else {
+            System.out.println("No has trobat cap carbassa");
+        }
     }
 
 }

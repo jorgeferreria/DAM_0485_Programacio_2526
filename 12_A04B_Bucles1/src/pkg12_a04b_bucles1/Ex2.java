2c75a06dbbff2d7f845e4bfc52b1a8bf0462d8a9
jrubio55 <registres17@gmail.com>
2025-11-05 20:15:27 +0100

    12_A04B_Bucles1 Act2 i 3

--- 12_A04B_Bucles1/src/pkg12_a04b_bucles1/Ex2.java
+++ 12_A04B_Bucles1/src/pkg12_a04b_bucles1/Ex2.java
@@ -1,8 +1,43 @@
 package pkg12_a04b_bucles1;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class Ex2 {
 
     public static void main(String[] args) {
+        Random rnd = new Random();
+        Scanner sc = new Scanner(System.in);
+        final int MAX_INTENTS = 5;
+        int numeroSecret, numeroProvar, intents = 0;
+        boolean numeroEncertat = false;
+
+        numeroSecret = rnd.nextInt(5, 51);
+        do {
+            System.out.print("Dis-me el número a provar: ");
+            intents++;
+            if (sc.hasNextInt()) {
+                numeroProvar = sc.nextInt();
+                if(numeroProvar > numeroSecret) {
+                    System.out.println("Massa alt!");
+                } else if(numeroProvar < numeroSecret){
+                    System.out.println("Massa baix!");
+                } else { // Encertat!
+                    System.out.println("Enhorabona, has encertat! i has utilitzat " 
+                            + intents + " intents");
+                    numeroEncertat = true;
+                }
+            } else {
+                System.out.println("Ha de ser un número!");
+            }
+            
+            // Buidar Buffer
+            sc.nextLine();
+        } while (!numeroEncertat && intents < MAX_INTENTS);        
+        
+        if(intents >= MAX_INTENTS){
+            System.out.println("Has perdut! Massa intents!!! el número era " + numeroSecret);
+        }
         
     }
     
--- 12_A04B_Bucles1/src/pkg12_a04b_bucles1/Ex3.java
+++ 12_A04B_Bucles1/src/pkg12_a04b_bucles1/Ex3.java
@@ -1,9 +1,27 @@
 package pkg12_a04b_bucles1;
 
+import java.util.Scanner;
+
 public class Ex3 {
 
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        char opcio = 0;
+        int totalIngredients = 0;
+        String ingredient, llista = "";
         
+        do {
+            System.out.print("Quin ingredient vols afegir a la poció? ");
+            ingredient = sc.nextLine();
+            llista += ++totalIngredients + ". "+ ingredient + "\n";
+            
+            System.out.println("Vols afegir un altre ingredient? (S/N)");
+            opcio = sc.nextLine().toLowerCase().charAt(0);
+            
+        } while (opcio == 's');
+        
+        System.out.println("\nIngredients: \n"+ llista + "Total d'ingredients: " 
+                + totalIngredients);
     }
     
 }

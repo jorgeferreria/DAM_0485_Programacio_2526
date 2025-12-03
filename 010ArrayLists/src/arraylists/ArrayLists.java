1e984532de333348726aa6bd60569ec4d34f1ef2
jrubio55 <registres17@gmail.com>
2025-12-03 20:05:28 +0100

    21_A01_Metodes comentaris

--- 010ArrayLists/src/arraylists/ArrayLists.java
+++ 010ArrayLists/src/arraylists/ArrayLists.java
@@ -6,31 +6,43 @@ public class ArrayLists {
 
     public static void main(String[] args) {
     
+        // CREACIÓ D'UN ARRAYLIST DE TIPUS STRING, SOLS TIPUS COMPLEXOS (Integer, Double, Boolean, ...)
         ArrayList<String> personas = new ArrayList();
         
+        // AFEGIR UN ELEMENT
         personas.add("Pepe");
         personas.add("Maria");
         personas.add("Jose");
+        
+        // AFEGIR UN ELEMENT EN UNA POSICIÓ DETERMINADA
         personas.add(0,"Laura");
         
+        // ELIMINAR UN ELEMENT D'UNA POSICIÓ DETERMINADA
         personas.remove(2);
         
+        // MODIFICAR UN ELEMENT D'UNA POSICIÓ DETERMINADA PER UN NOU VALOR
         personas.set(1, "Andres");
         
+        // BUSCAR UN ELEMENT, RETORNA TRUE/FALSE
         System.out.println(personas.contains("Luis"));
         System.out.println(personas.contains("Laura"));
+        
+        // BUSCAR UN ELEMENT, RETORNA -1 SI NO EL TROVA O LA POSCICIÓ ON TROVA LA PRIMERA OCURRENCIA
         System.out.println(personas.indexOf("Luis"));
         System.out.println(personas.indexOf("Laura"));
         
+        // ELIMINAR TOTS ELS ELEMENTS
 //        personas.clear();
         
         System.out.println(personas);
         System.out.println(personas.size());
         
         for (int i = 0; i < personas.size(); i++) {
+            // MOSTRAR UNA POSICIÓ DETERMINADA
             System.out.println((i+1) + ". " + personas.get(i));
         }
         
+        // FOR-EACH PER RECORRER COLLECIONS
         for (String persona : personas) {
             System.out.println(persona);
         }
--- 21_A02_MetodesString/src/pkg21_a02_metodesstring/Main.java
+++ 21_A02_MetodesString/src/pkg21_a02_metodesstring/Main.java
@@ -4,6 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
         
+        String[] parelles;        
         String csv = 
                 "Sr. Jimenez,Dra. Garcia,Dr. Ruiz,Sra. Gonzalez,Dra. Hernandez,"
                 + "Sr. Rodriguez,Sr. Diaz,Dr. Fernandez,Dra. Moreno,Sr. Lopez,"
@@ -11,7 +12,8 @@ public class Main {
                 + "Sra. Perez,Sr. Alonso,Dr. Gomez,Sra. Gutierrez,Dr. Martin,"
                 + "Dra. Navarro,Sr. Torres";
 
-        
+        // CONVERTIR EL STRING EN ARRAY
+        parelles = csv.split(",");
         
         
     }
